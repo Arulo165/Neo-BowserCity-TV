d05947345c4048fbd94d7814d8ddbc81607fd4a9
Arulo165 <arulomodding@gmail.com>
2024-11-24 16:28:56 +0100

    Added functions etc

---
+++ setup.py
@@ -0,0 +1,8 @@
+from cx_Freeze import setup, Executable
+
+setup(
+    name="Neo Bowser City TV",
+    version="1.0",
+    description="Ein cooles Programm",
+    executables=[Executable("Neo Bowser City TV.py")]# icon="Data/icons/icon.ico"
+)
